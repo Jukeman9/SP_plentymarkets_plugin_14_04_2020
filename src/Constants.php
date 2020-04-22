c3abeed70d449bc11433408edb99ead478218dfd
Jukeman9 <51355966+Jukeman9@users.noreply.github.com>
2020-04-22 15:02:51 +0200

    Add files via upload

---
+++ src/Constants.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace SwiatPrzesylek;
+
+
+class Constants
+{
+    const PLUGIN_NAME = 'SwiatPrzesylek';
+    const ENV_PROD = 'prod';
+    const ENV_DEV = 'dev';
+
+    const VOLUME_TYPE_FROM_SHIPPING_PACKAGE = 0;
+    const VOLUME_TYPE_FROM_ITEMS_DATA = 1;
+
+}
